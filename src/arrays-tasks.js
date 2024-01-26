8fa484264f7ab5dbd290154b9a0aadd394cfdd5d
SzymonSurowka <szymon121122@gmail.com>
2024-01-26 23:03:29 +0100

    init: task update

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,9 +20,15 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    throw new Error('Both start and end must be numbers.');
+  }
+  const length = Math.abs(end - start) + 1;
+  const intervalArray = Array.from({ length }, (_, index) => start + index);
+  return intervalArray;
 }
+getIntervalArray(1, 5);
 
 /**
  * Returns a new array where each element is the sum of the corresponding elements
@@ -37,9 +43,18 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+
+  const summedArray = Array.from({ length: maxLength }, (_, index) => {
+    const sum = (arr1[index] || 0) + (arr2[index] || 0);
+    return sum;
+  });
+
+  return summedArray;
 }
+
+sumArrays([1, 2, 3], [4, 5, 6]);
 
 /**
  * Returns an index of the specified element in array or -1 if element is not found.
@@ -53,8 +68,8 @@ function sumArrays(/* arr1, arr2 */) {
  *    findElement(['Array', 'Number', 'string'], 'Date') => -1
  *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
 
 /**
